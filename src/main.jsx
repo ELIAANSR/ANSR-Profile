c9c1deab06b95bbec6c076f6cfccabc98e8b58bb
ELIAANSR <alex@eliaheals.com>
2026-03-11 10:50:20 +0100

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ANSRProfile from './ANSRProfile.jsx'
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ANSRProfile />
+  </React.StrictMode>,
+)
