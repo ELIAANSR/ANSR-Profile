c0388ec319a219c143c2644076ae318544d03bf7
ELIAANSR <alex@eliaheals.com>
2026-03-11 10:47:58 +0100

    main.jsx

--- main.jsx
+++ main.jsx
@@ -7,3 +7,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ANSRProfile />
   </React.StrictMode>,
 )
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ANSRProfile from './ANSRProfile.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ANSRProfile />
+  </React.StrictMode>,
+)
